b1a0b4b1ca245324fa051b2ff48e984092d758aa
Hien Phan <tpm.phanconghien@gmail.com>
2020-02-12 17:40:23 +0700

    Add launch 2 apps sample

---
+++ src/main/java/AppiumServer.java
@@ -0,0 +1,59 @@
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import org.apache.tools.ant.types.LogLevel;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class AppiumServer {
+
+    private static AppiumServer INSTANCE;
+    private AppiumDriverLocalService service;
+
+    private AppiumServer() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        AppiumServiceBuilder builder = new AppiumServiceBuilder();
+        builder.withIPAddress("127.0.0.1");
+        builder.usingPort(4723);
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.LOG_LEVEL, LogLevel.INFO.toString());
+        builder.withLogFile(createLoggerFile());
+        service = AppiumDriverLocalService.buildService(builder);
+    }
+
+    public static AppiumServer getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new AppiumServer();
+        }
+        return INSTANCE;
+    }
+
+    void startServer() {
+        if (service != null && !service.isRunning()) {
+            service.start();
+        }
+    }
+
+    void stopServer(){
+        if (service != null && !service.isRunning()) {
+            service.stop();
+        }
+    }
+
+    private File createLoggerFile() {
+        // Specific Log File Path
+        String fileName = new SimpleDateFormat("dd-MM-yyyy HHmm'.txt'").format(new Date());
+        File logDirectory = new File("logs/");
+        if (!logDirectory.exists()) {
+            logDirectory.mkdir();
+        }
+        return new File(logDirectory, fileName);
+    }
+
+}
---
+++ src/test/java/LaunchTwoAppsTest.java
@@ -0,0 +1,67 @@
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SampleTest {
+    private IOSDriver driver;
+    private AppiumServer appiumServer;
+
+    public DesiredCapabilities getDefaultDesiredCapabilities() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "YourDeviceName");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.3");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        capabilities.setCapability(MobileCapabilityType.UDID, "d2eaf15d6eb6009c2576dd63add4ae4b495347dd");
+
+        return capabilities;
+    }
+
+    public void launchSettingsApp() {
+        try {
+            DesiredCapabilities capabilities = getDefaultDesiredCapabilities();
+            capabilities.setCapability(MobileCapabilityType.APP, "Settings");
+            driver = new IOSDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void launchNotesApp() {
+        try {
+            DesiredCapabilities capabilities = getDefaultDesiredCapabilities();
+            capabilities.setCapability(MobileCapabilityType.APP, "com.apple.mobilenotes");
+            driver = new IOSDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setUp() {
+        appiumServer = AppiumServer.getInstance();
+        appiumServer.startServer();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+        appiumServer.stopServer();
+    }
+
+    @Test
+    public void testLaunch2Apps() {
+        launchSettingsApp();
+        launchNotesApp();
+        launchSettingsApp();
+    }
+}
